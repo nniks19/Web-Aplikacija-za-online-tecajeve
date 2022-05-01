c7caadc22de18678e333f7dd5f06cd7f17db3cb5
nniks19 <77620949+nniks19@users.noreply.github.com>
2022-05-01 18:10:19 +0200

    update
    Connection to base now works :)

---
+++ WAZOT/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAZOT.Models;
+
+namespace WAZOT.Data;
+
+public class ApplicationDbContext:DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+
+    }
+    public DbSet<Razina_Prava> RazinePrava { get; set; }
+}
---
+++ WAZOT/Models/Razina_Prava.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WAZOT.Models
+{
+    public class Razina_Prava
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Naziv { get; set; }
+
+    }
+}
